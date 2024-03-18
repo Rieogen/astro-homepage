b51b4acd9f1faa822826b826beb92afcb117c970
koto74 <kotoganaiyo@gmail.com>
2024-03-18 22:49:18 +0900

    fix: tagページのエラーを修正

---
+++ src/data/post.ts
@@ -0,0 +1,11 @@
+import { getCollection } from 'astro:content';
+
+export const getAllPosts = async () => {
+  return await getCollection('post')
+}
+
+export const getUniqueTags = async () => {
+  const allPosts = await getCollection('post')
+  const allTags = allPosts.flatMap((post) => [...post.data.tags])
+  return [...new Set(allTags)]
+}
