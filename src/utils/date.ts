9bd064f663ec80a0bf94c9fabf1250a37aaf7d5b
koto74 <kotoganaiyo@gmail.com>
2024-03-20 12:48:00 +0900

    chore: apply format

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,9 @@
-const dateFormat = new Intl.DateTimeFormat('en-GB', {
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric'
+const dateFormat = new Intl.DateTimeFormat("en-GB", {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
 });
 
 export const getFormatDate = (date: Date) => {
-  return dateFormat.format(new Date(date))
-}
+  return dateFormat.format(new Date(date));
+};
