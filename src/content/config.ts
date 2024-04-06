c0fe2411164cf9eb167cea8456b9b91ff96fe300
koto74 <kotoganaiyo@gmail.com>
2024-04-06 21:36:12 +0900

    feat: projectCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,10 +7,22 @@ const postCollection = defineCollection({
     publishDate: z.date(),
     description: z.string(),
     tags: z.array(z.string()),
-    image: z.string().optional(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    stack: z.array(z.string()),
+    website: z.string().optional(),
+    github: z.string(),
   }),
 });
 
 export const collections = {
   post: postCollection,
+  projects: projectCollection,
 };
